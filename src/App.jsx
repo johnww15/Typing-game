5696819e8a768e2747b7b0987aac3a23e7a79ca5
Jonathan Q <jonathan.quekww@gmail.com>
2024-06-11 01:40:44 +0800

    resolved syntax error in mockdata

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // import { useState } from 'react'
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
 import Input from "./components/Input";
 import Game from "./components/Game";
 
--- src/components/FixedWords.jsx
+++ src/components/FixedWords.jsx
@@ -1,4 +1,8 @@
+import { MOCK_DATA } from "../data/MockData";
+
 export default function FixedWords() {
+  console.log(MOCK_DATA);
+
   return (
     <>
       <h1>FixedWords</h1>
