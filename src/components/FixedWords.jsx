713a5395145e7373ca68e7a618e02a88eadf3b46
Jonathan Q <jonathan.quekww@gmail.com>
2024-06-13 01:19:04 +0800

    included base handle change function to keep track of textarea inputs for later

--- src/components/FixedWords.jsx
+++ src/components/FixedWords.jsx
@@ -5,11 +5,16 @@ export default function FixedWords() {
   const [fixedGameWords, setFixedGameWords] = useState([]);
   // inputWords state gets it's "proper" initial state from the useEffect function below
   const [inputWords, setInputWords] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   // Create function to randomise and generate array of words for the game
   const getRandomWords = (array, count) => {
     const shuffledArray = array.slice().sort(() => Math.random() - 0.5);
     return shuffledArray.slice(0, Math.min(count, shuffledArray.length));
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
   };
 
   useEffect(() => {
@@ -24,7 +29,7 @@ export default function FixedWords() {
       <h1>FixedWords</h1>
       <div>
         <p data-testid="fixedwords">{fixedGameWords.join(" ")}</p>
-        <textarea />
+        <textarea value={inputWords[currentIndex]} onChange={handleChange} />
       </div>
     </>
   );
